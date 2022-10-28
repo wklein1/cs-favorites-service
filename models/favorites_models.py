1b7786d263cd3f2610c69061367750315623804d
wklein1 <Witali@>
2022-10-28 17:12:04 +0200

    #7 add favorites models

---
+++ models/favorites_models.py
@@ -0,0 +1,14 @@
+from models.custom_base_model import CustomBaseModel
+
+def FavoritesModel(CustomBaseModel):
+    key: str = Field(alias="favoritesListId")
+    owner_id: str
+    component_ids: list[str]
+    product_ids: list[str]
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+class ToggleFavoriteModel(CustomBaseModel):
+    owner_id: str
+    to_favorites_id: str
